4f55b91772f27ce35cf2e7333614812e18d7d3bb
KarenLarens <17151290@aguascalientes.tecnm.mx>
2022-08-03 12:49:23 -0500

    Sumando con codigo java

--- 01.js
+++ 01.js
@@ -12,20 +12,48 @@ const app = new Vue({
             {nombre:'Platano', cantidad: 10}
 
         ],
-        nuevaFruta:''
+        nuevaFruta:'',
+        nuevoNumero:0,
+        total:0
 
     },
     //Metodo que guarda la informacion de un input detro de un arreglo
     methods:{
         agregarFruta(){
+            //El metodo push guarda la informacion del on click dentro de el objeto fruit 
             this.fruit.push({
                 nombre: this.nuevaFruta, cantidad:0
 
-            })
+            });
+            //Se agrega esta linea para poder eliminar el valor que queda guardado en el input 
+            this.nuevaFruta='';
+        },
+        agregarNumero(){
+            //El metodo push guarda la informacion del on click dentro de el objeto fruit 
+            this.fruit.push({
+                cantidad: this.nuevoNumero, nombre:'Fruta default'
+
+            });
+            //Se agrega esta linea para poder eliminar el valor que queda guardado en el input 
+            this.nuevoNumero='';
             
+        }
+        },
+        
+        //suma todas las cantidades de las frutas
+        computed:{
+            sumarFrutas(){
+                this.total=0;
+                for(fruta of this.fruit){
+                    this.total=this.total + fruta.cantidad;
+
+                }
+                return this.total;
+            }
 
         }
 
+
     }
 
-})
+)
